9b9e1ecf5d3a8d42928700c5d153bcc77fa0876c
rroblf01 <rroblf01@estudiantes.unileon.es>
2019-05-01 11:02:14 +0200

    Add files via upload

--- src/practica7/Main.java
+++ src/practica7/Main.java
@@ -6,25 +6,53 @@ import java.util.ArrayList;
 public class Main {
 
 	public static void main(String[] args) {
-		String alf="aábcdeéfghiíjklmnñoópqrstuúvwxyzAÁBCDEÉFGHIÍJKLMNÑOÓPQRSTUÚVWXYZ0123456789 ,.:-()";
-		//1º 101400389190
-		//2º a) [66,50,48,17,26,45,41,33,63]
-		//	 b) 2404201924042019
-		
+
 		PotenciacionModular potencia = new PotenciacionModular();
 		BigInteger a = new BigInteger("2424242424");
 		BigInteger b = new BigInteger("4444444444");
 		BigInteger n = new BigInteger("201920192019");
 		BigInteger respuesta1 = potencia.potenciaMod(a,b,n);
-		//System.out.println(respuesta1.toString());
+		System.out.println(respuesta1.toString());
 		
 		BigInteger numero = new BigInteger("123456789123456789");
 		BigInteger orden = new BigInteger("81");
 		ArrayList<String> resultado = potencia.obtenerEnteros(numero, orden);
+                
 		for(String frase: resultado) {
-			System.out.println(frase);
+			System.out.print(frase+ " ");
 		}
+                System.out.println("");
+                
+                int[] lista={1,24,7,46,30,48,79,51,53};
+                BigInteger[] listaBig = new BigInteger[lista.length];
+                for(int i = 0; i < lista.length; i++){
+                    listaBig[i] = new BigInteger(""+lista[i]);
+                }
+                BigInteger base = new BigInteger("81");
+                
+                BigInteger solucion = potencia.obtenerNumero(listaBig, base);
+                System.out.println(solucion.toString());
+                
+                
+                String frase = "24 de abril";
+                String alf = "aÃ¡bcdeÃ©fghiÃ­jklmnÃ±oÃ³pqrstuÃºvwxyzAÃBCDEÃ‰FGHIÃJKLMNÃ‘OÃ“PQRSTUÃšVWXYZ0123456789 ,.:-()";
+                int modulo = 81;
+                
+                BigInteger ejercicio21 = potencia.textoAEntero(frase,alf,modulo);
+                System.out.println(ejercicio21.toString());
+                
+                BigInteger textoEnNumero = new BigInteger("1000000");
+               String ejercicio22  = potencia.enteroATexto(textoEnNumero,base,alf);
+               System.out.println(ejercicio22);
 		
+                BigInteger p= new BigInteger("428765575697899393132131414307");
+                BigInteger g= new BigInteger("2");
+                String A="bÃº32a4Ã2lDÃ­hUeÃ‰B";
+                String B="Ã¡uÃTJKWz7Ã‰ I.Ã‘2Ã‰";
+                BigInteger b3= new BigInteger("1717171717171717");
+               
+               String ejercicio3 = potencia.Hellman(A,B,b3,g,p,alf);
+               System.out.println("Ejercicio3: "+ejercicio3);
 	}
 	
 	
--- src/practica7/PotenciacionModular.java
+++ src/practica7/PotenciacionModular.java
@@ -10,8 +10,7 @@ public class PotenciacionModular {
 	
 	public BigInteger potenciaMod(BigInteger a, BigInteger b, BigInteger n) {
 		char[] bChar = b.toString(2).toCharArray();
-		
-		BigInteger Aaux = a;
+
 		Integer aux = 1;
 		BigInteger m = BigInteger.valueOf(aux.intValue());
 		
@@ -27,28 +26,74 @@ public class PotenciacionModular {
 		return m;
 	}
 	
-	public static ArrayList<String> obtenerEnteros(BigInteger numero, BigInteger orden){
-			
+	public static ArrayList<String> obtenerEnteros(BigInteger numero, BigInteger orden){			
 		   ArrayList<String> binario = new ArrayList<String>();
 		   BigInteger resto;
-		   String binarioString = "";
 
-		   do{
-                    
+		   do{       
 		      resto = numero.mod(orden);
 		      numero = numero.divide(orden);
 		      binario.add(resto.toString());
 		   }while(numero.compareTo(orden) >= 0); //Haremos el bucle hasta que el cociente no se pueda dividir mas
 
 		   binario.add(numero.toString()); //Cogeremos el ultimo cociente
-
-		   //Cogemos cada uno de los elementos del ArrayList y los juntamos en un String
-		   for(int i = 0; i < binario.size(); i++){
-		       binarioString += binario.get(i);
-		   }
 			
 		   return binario;
 	}
+        
+       public BigInteger obtenerNumero(BigInteger[] enteros, BigInteger base){
+           BigInteger resultado = new BigInteger("0");
+           int posicion = 0;
+           for(int i = enteros.length-1; i >= 0 ; i--){
+               
+               resultado = resultado.add(enteros[posicion].multiply(base.pow(i)));
+               posicion++;
+           }
+           
+           return resultado;
+       }
 	
 
+       public BigInteger textoAEntero(String frase, String alf, int modulo){
+         char[] fraseChar = frase.toCharArray();
+           char[] alfChar = alf.toCharArray();
+           int[] listaNumeros = new int[fraseChar.length];
+           for(int i = 0; i < fraseChar.length; i++){
+               for(int j = 0; j < alfChar.length; j++){
+                   if(fraseChar[i] == alfChar[j]){
+                       listaNumeros[i] = j;
+                       break;
+                   }
+               }
+           }
+
+           BigInteger[] listaBig = new BigInteger[listaNumeros.length];
+                for(int i = 0; i < listaNumeros.length; i++){
+                    listaBig[i] = new BigInteger(""+listaNumeros[i]);
+                }
+            BigInteger base = new BigInteger(""+modulo);
+           
+           return obtenerNumero(listaBig,base);
+       }
+       
+       public String enteroATexto(BigInteger entero, BigInteger base, String alf){
+           String frase = "";
+           char[] alfChar = alf.toCharArray();
+           ArrayList<String> posiciones = obtenerEnteros(entero, base);
+           for(String posicionLetras: posiciones){
+               frase += alfChar[Integer.parseInt(posicionLetras)];
+           }
+           frase =  new StringBuffer(frase).reverse().toString();
+           return frase;
+       }
+       
+       public String Hellman(String A, String B,BigInteger b, BigInteger g,BigInteger p,  String alf){
+           
+           BigInteger ANumerico = textoAEntero(A,alf,alf.length());
+           BigInteger modulo = new BigInteger(""+alf.length());
+           BigInteger clave =  ANumerico.modPow(b, p);
+
+           String frase = enteroATexto(clave, modulo,alf);
+           return frase;
+       }
 }
